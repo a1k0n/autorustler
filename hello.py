353821af421055267c1ab8d9b5e6d1afb5b55fa0
Andy Sloane <andy@a1k0n.net>
2013-12-06 00:28:40 -0600

    Read 2-channel PWM input, make it available on SPI
    I can't believe I got this to work with such small jitter.  It bounces
    around by about 10-15 counts but is usually pretty dead-on.  I might do
    a histogram and add an input filter.
    
    Something is a little funky electrically: it only works if pullups are
    enabled, which leads me to believe either the radio is open collector or
    its output impedance is pretty high.  Nevertheless, with my 10k/10k
    resistor divider, it works.  I need a scope to see what's actually going
    on, but I don't think I can possibly be exceeding 3.3V on the input
    pins at least.

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-
 import spidev
+import time
+import sys
+
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
 spi.bits_per_word = 8
-spi.max_speed_hz = 1500000
+#spi.max_speed_hz = 1500000
+spi.max_speed_hz = 500000
 
-print ['%02x' % x for x in spi.xfer2([0x01, 0, 0]*4)]
+while True:
+    #print ['%02x' % x for x in spi.xfer2([0x01, 0, 0]*4)]
+    data = spi.xfer2([0x02, 0, 0, 0, 0])
+    ch1 = data[1] + (data[2]<<8)
+    ch2 = data[3] + (data[4]<<8)
+    sys.stdout.write("\r%5d %5d" % (ch1, ch2))
+    sys.stdout.flush()
+    time.sleep(0.2)
--- main.c
+++ main.c
@@ -3,22 +3,69 @@
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 
-static uint8_t spi_state = 0;
-static uint8_t spi_hibyte;
+volatile static uint16_t ch1_counts = 0, ch2_counts = 0;
+ISR(PCINT0_vect) {
+  static uint16_t ch1_start = 0, ch2_start = 0;
+  static uint8_t ch1_state = 0, ch2_state = 0;
+  uint16_t tm = TCNT1L;
+  uint8_t state = PINB;
+  tm |= (uint16_t)TCNT1H << 8;
+  if ((state&1) != ch1_state) {
+    ch1_state = state&1;
+    if (ch1_state) {
+      ch1_start = tm;
+    } else {
+      ch1_counts = tm - ch1_start;
+    }
+  }
+  if ((state&2) != ch2_state) {
+    ch2_state = state&2;
+    if (ch2_state) {
+      ch2_start = tm;
+    } else {
+      ch2_counts = tm - ch1_start;
+    }
+  }
+}
+
+// dumb int-on-pin-change pwm capture
+static void pwmcapture_init() {
+  PORTB = 0x03;  // add pull-ups to inputs
+  PCMSK0 = 0x03;
+  PCICR = 0x01;
+}
+
 ISR(SPI_STC_vect) {
   char spi_in = SPDR;
+  static uint8_t spi_state = 0;
+  static uint8_t spi_hibyte;
   switch (spi_state) {
     default:  // command mode, state 0 (or invalid states)
       if (spi_in == 0x01) {  // command 0x01: read timer
         SPDR = TCNT1L;
         spi_hibyte = TCNT1H;
         spi_state = 1;
+      } else if(spi_in == 0x02) {  // command 0x02: read channels
+        SPDR = ch1_counts&255;
+        spi_state = 2;
       } else {
         SPDR = 0;
       }
       break;
     case 1: // send one high byte, then return to state 0
       SPDR = spi_hibyte;
+      spi_state = 0;
+      break;
+    case 2: // read counts, ch1 hi
+      SPDR = ch1_counts >> 8;
+      spi_state++;
+      break;
+    case 3: // read counts, ch2 lo
+      SPDR = ch2_counts & 255;
+      spi_state++;
+      break;
+    case 4: // read counts, ch2 hi
+      SPDR = ch2_counts >> 8;
       spi_state = 0;
       break;
   }
@@ -39,6 +86,7 @@ static void spi_init() {
 int main() {
   tmr1_init();
   spi_init();
+  pwmcapture_init();
 
   sei();
 
