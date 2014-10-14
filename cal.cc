bccd0dd075cacfefafb45f05a7e4928f335a9f78
Andy Sloane <andy@a1k0n.net>
2014-10-14 15:50:39 -0500

    convert to Eigen

--- cal.cc
+++ cal.cc
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <math.h>
-
+#include <Eigen/Dense>
 #include "imu/imu.h"
+
+using Eigen::Vector3f;
 
 volatile bool done = false;
 
@@ -27,9 +29,11 @@ int main() {
   IMUState s;
   const int period = 50000;
   const float gain = 1.0;
-  float b[3] = {0, 0, 0};
+  Vector3f b(0, 0, 0);
   imu_read(i2cfd, &s);
-  float lastm[3] = {s.mag_x, s.mag_y, s.mag_z};
+
+  const Vector3f gyromean(49.68937876, -31.1743487, -16.9739479);
+  Vector3f lastm(s.mag_x, s.mag_y, s.mag_z);
   while (!done) {
     timeval tv0;
     gettimeofday(&tv0, NULL);
@@ -43,24 +47,17 @@ int main() {
     // m' ~ N(m + bm, sigmam)
 
     // float m[3] = {s.mag_x - b[0], s.mag_y - b[1], s.mag_z - b[2]};
-    float m[3] = {s.mag_x - b[0], s.mag_y - b[1], s.mag_z - b[2]};
-    float dm[3] = {m[0] - lastm[0], m[1] - lastm[1], m[2] - lastm[2]};
-    float dmmag = dm[0]*dm[0] + dm[1]*dm[1] + dm[2]*dm[2];
+    Vector3f m(s.mag_x - b[0], s.mag_y - b[1], s.mag_z - b[2]);
+    Vector3f dm(m - lastm);
+    float dmmag = dm.norm();
     if (dmmag > 10) {
-      float mag2 = sqrt(m[0]*m[0] + m[1]*m[1] + m[2]*m[2]);
-      float mag1 = sqrt(lastm[0]*lastm[0] + lastm[1]*lastm[1] +
-                        lastm[2]*lastm[2]);
-      float scale = gain * (mag2 - mag1) / sqrt(dmmag);
-      b[0] += dm[0] * scale;
-      b[1] += dm[1] * scale;
-      b[2] += dm[2] * scale;
+      float scale = gain * (m.norm() - lastm.norm()) / dmmag;
+      b += scale * dm;
       printf("b:[%f %f %f] m:[%f %f %f]\n", b[0], b[1], b[2],
              m[0], m[1], m[2]);
     }
 
-    lastm[0] = m[0];
-    lastm[1] = m[1];
-    lastm[2] = m[2];
+    lastm = m;
 
     timeval tv;
     gettimeofday(&tv, NULL);
